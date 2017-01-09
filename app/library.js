6c4bb665b88ef195f716cfce570159b2df616c13
DAROT <darotudeen@gmail.com>
2017-01-08 22:17:20 -0800

    commiting a completedly passed aritGeo

---
+++ app/library.js
@@ -0,0 +1,39 @@
+'use strict'
+
+function dataTypes(arg){
+	if ( arg.constructor === String ){
+		return arg.length
+	}
+	if(typeof(arg) === "object"){
+		return "no value"
+	}
+	if (typeof arg === "boolean"){
+		return arg.valueOf()
+	}
+	if(arg.constructor === Number){
+		if(arg < 100){
+			return "less than 100"
+		}
+		if(arg > 100){
+			return "more than 100"
+		}
+		return "equal to 100"
+	}
+	if (arg.constructor === Array){
+		if(!arg[2]){
+			return "undefined"
+		}
+		return arg[2]
+	}
+	if(typeof(arg) === "function"){
+		return arg(true)
+	}
+	if(arg === ''){
+		return 0
+	}
+	
+}
+
+module.exports = {
+	dataTypes : dataTypes
+};
---
+++ tests/tests.js
@@ -0,0 +1,64 @@
+'use strict';
+require('jasmine');
+// var assert = chai.assert;
+// var expect = chai.expect;
+// var should = chai.should();
+var myApp = require('../app/library.js');
+
+describe("Data types tests ", function() {
+  describe("Case for some falsy values", function() {
+
+    it("should return 'no value' for null", function() {
+      expect(myApp.dataTypes(null)).toEqual('no value');
+    });
+
+    it("should return 'no value' for undefined", function() {
+      expect(myApp.dataTypes(undefined)).toEqual('no value');
+    });
+
+  });
+
+  describe("Case for booleans", function() {
+
+    it("should return true for `true`", function() {
+      expect(myApp.dataTypes(true)).toBe(true);
+    });
+
+    it("should return false for `false`", function() {
+      expect(myApp.dataTypes(false)).toBe(false);
+    });
+
+  });
+
+  describe("Case for Numbers", function() {
+
+    it("should return 'less than 100' for 44", function() {
+      expect(myApp.dataTypes(44)).toBe('less than 100');
+    });
+
+    it("should return 'more than 100' for 144", function() {
+      expect(myApp.dataTypes(144)).toBe('more than 100');
+    });
+
+    it("should return 'equal to 100' for 100", function() {
+      expect(myApp.dataTypes(100)).toBe('equal to 100');
+    });
+
+
+  });
+
+  describe("Case for Strings", function() {
+
+    it("should return the length of `tergiversate`", function() {
+      expect(myApp.dataTypes('tergiversate')).toBe(12);
+    });
+
+    it("should return the length of an empty string", function() {
+      expect(myApp.dataTypes('')).toBe(0);
+    });
+
+    it("should return the length of `555`", function() {
+      expect(myApp.dataTypes('555')).toBe(3);
+	});
+  });
+});
